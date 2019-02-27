f9c2958ffcc5478ca41dab9e72fb01b5366459ca
Reed Espinosa <reed.espinosa@nasa.gov>
2019-02-27 11:02:35 -0500

    cleaning up script

--- GRASPfromNetCDF.py
+++ GRASPfromNetCDF.py
@@ -8,15 +8,16 @@ from datetime import datetime as dt
 import warnings
 import os
 import sys
-sys.path.append(os.path.join("..", "GRASP_PythonUtils"))
-from runGRASP import graspRun, pixel
+sys.path.append(os.path.join("..", "GRASP_scripts"))
+from runGRASP import graspDB, graspRun, pixel
 
 # Paths to files
 basePath = '/Users/wrespino/Synced/' # NASA MacBook
 #basePath = '/home/respinosa/ReedWorking/' # Uranus
 dayStr = '20060901'
-dirGRASPworking = os.path.join(basePath, 'Remote_Sensing_Projects/MADCAP_CAPER/graspWorking') # path to store GRASP SDATA and output files
-pathYAML = os.path.join(basePath, 'Remote_Sensing_Analysis/GRASP_PythonUtils/settings_HARP_16bin_6lambda.yml') # path to GRASP YAML file
+#dirGRASPworking = os.path.join(basePath, 'Remote_Sensing_Projects/MADCAP_CAPER/graspWorking') # path to store GRASP SDATA and output files
+dirGRASPworking = False # use sytem temp directories as path to store GRASP SDATA and output files 
+pathYAML = os.path.join(basePath, 'Local_Code_MacBook/MADCAP_Analysis/YAML_settingsFiles/settings_HARP_16bin_6lambda.yml') # path to GRASP YAML file
 #radianceFNfrmtStr = os.path.join(basePath, 'Remote_Sensing_Projects/MADCAP_CAPER/sept01_testCase/calipso-g5nr.vlidort.vector.MCD43C.'+dayStr+'_00z_%dd00nm.nc4')
 #radianceFNfrmtStr = os.path.join(basePath, 'Remote_Sensing_Projects/MADCAP_CAPER/sept01_testCase_regenerated/calipso-g5nr.vlidort.vector.MCD43C.'+dayStr+'_00z_%dd00nm.nc4')
 radianceFNfrmtStr = os.path.join(basePath, 'Remote_Sensing_Projects/MADCAP_CAPER/sept01_testCase_regenerated/calipso-g5nr.vlidort.vector.MCD43C_noBPDF.'+dayStr+'_00z_%dd00nm.nc4')
@@ -26,22 +27,26 @@ lndCvrFN = os.path.join(basePath, 'Remote_Sensing_Projects/MADCAP_CAPER/sept01_t
 
 # Constants
 #wvls = [0.410, 0.440, 0.470, 0.550, 0.670, 0.865, 1.020, 1.650, 2.100] # wavelengths to read from levC files
-#wvls = [0.440, 0.470, 0.550, 0.670, 0.865, 1.020] # wavelengths to read from levC files
 wvls = [0.440, 0.550, 0.670, 0.865, 1.020, 2.100] # wavelengths to read from levC files
-#wvls = [1.020, 2.100] # wavelengths to read from levC vlidort files
 wvlsLidar = [0.532, 1.064] # wavelengths to read from levC lidar files
 dateRegex = '.*([0-9]{8})_[0-9]+z.nc4$' # regex to pull date string from levBFN, should give 'YYYYMMDD'
 scaleHght = 7640 # atmosphere scale height (meters)
 stndPres = 1.01e5 # standard pressure (Pa)
-lndPrct = 100; # land cover amount (%), ocean only for now
-grspChnkSz = 300 # number of pixles in a single SDATA file
+lndPrct = 100; # land cover amount (%), land only for now
+grspChnkSz = 3 # number of pixles in a single SDATA file
 orbHghtKM = 700 # sensor height (km)
-
 GRASP_MIN = 1e-6 # SDATA measurements smaller than GRASP_MIN will be replaced by GRASP_MIN
+graspInputs = 'IQU' # 'Ionly' (intensity), 'DOLP' (I & DOLP) or 'IQU' (1st 3 stokes)
+maxCPUs = 2; # maximum number of simultaneous grasp run threads
+binPathGRASP = '/usr/local/bin/grasp' # path to grasp binary
+savePath = '/Users/wrespino/Desktop/MADCAP_test.pkl'
+
+# Variable to read in from radiance netCDF file, note that many variables are hard coded below
+# Also, this currently stores wavelength independent data Nwvlth times but the method is simple
+varNames = ['ROT', 'I', 'Q', 'U', 'surf_reflectance', 'surf_reflectance_Q', 'surf_reflectance_U', 'toa_reflectance', 'solar_zenith', 'solar_azimuth', 'sensor_zenith', 'sensor_azimuth', 'time','trjLon','trjLat']
+
 
 # Read in radiances, solar spectral irradiance and find reflectances
-# Currently stores wavelength independent data Nwvlth times but this method is simple...
-varNames = ['ROT', 'I', 'Q', 'U', 'surf_reflectance', 'surf_reflectance_Q', 'surf_reflectance_U', 'toa_reflectance', 'solar_zenith', 'solar_azimuth', 'sensor_zenith', 'sensor_azimuth', 'time','trjLon','trjLat']
 datStr = re.match(dateRegex, levBFN).group(1)
 dayDtNm = dt.strptime(datStr, "%Y%m%d").toordinal()
 Nwvlth = len(wvls)
@@ -60,16 +65,16 @@ warnings.simplefilter('always')
 for i in range(Nwvlth):
     for varName in np.setdiff1d(varNames, 'sensor_zenith'):
         measData[i][varName] = np.delete(measData[i][varName], invldInd, axis=0)
+    measData[i]['dtNm'] = dayDtNm + measData[i]['time']/86400
     measData[i]['DOLP'] = np.sqrt(measData[i]['Q']**2+measData[i]['U']**2)/measData[i]['I']
     measData[i]['I'] = measData[i]['I']*np.pi # GRASP "I"=R=L/FO*pi 
     measData[i]['Q'] = measData[i]['Q']*np.pi 
     measData[i]['U'] = measData[i]['U']*np.pi 
+    # HACK to only retrieve surface
 #    measData[i]['I'] = measData[i]['surf_reflectance']*np.cos(measData[i]['solar_zenith']*np.pi/180).reshape(-1,1) # GRASP "I"=R=L/FO*pi
 #    measData[i]['Q'] = measData[i]['surf_reflectance_Q']*np.cos(measData[i]['solar_zenith']*np.pi/180).reshape(-1,1) 
 #    measData[i]['U'] = measData[i]['surf_reflectance_U']*np.cos(measData[i]['solar_zenith']*np.pi/180).reshape(-1,1)
 #    measData[i]['DOLP'] = np.sqrt(measData[i]['Q']**2+measData[i]['U']**2)/measData[i]['I']
-    measData[i]['dtNm'] = dayDtNm + measData[i]['time']/86400
-
 
 
 # Read in levelB data to obtain pressure and then surface altitude
@@ -81,67 +86,74 @@ surfPres = np.delete(surfPres, invldInd)
 maslTmp = [scaleHght*np.log(stndPres/PS) for PS in surfPres]
 for i in range(Nwvlth): measData[i]['masl'] = maslTmp
 
+
 # Generate GRASPruns from cases
-#msTyp = np.r_[41, 44] # reflectance, DOLP; currently assumes all msTyp's have same nbvm
-msTyp = np.r_[41, 42, 43] # reflectance, Q, U; currently assumes all msTyp's have same nbvm
 graspObjs = []
-nip = msTyp.shape[0]
 Npix = measData[0]['I'].shape[0]
+Npix = 6 # HACK to test with only 6 pixels
 strtInds = np.r_[0:Npix:grspChnkSz]
-#strtInds = [0] # HACK!!!
 for strtInd in strtInds:
     gObj = graspRun(pathYAML, orbHghtKM, dirGRASPworking)
     endInd = min(strtInd+grspChnkSz, Npix)
-#    endInd = strtInd+10 # HACK!!!
     for ind in range(strtInd, endInd):
         dtNm = measData[0]['dtNm'][ind]
         lon = measData[0]['trjLon'][ind]
         lat = measData[0]['trjLat'][ind]
         masl = max(measData[0]['masl'][ind], -100) # defualt GRASP build complains below -100m  
-#        masl = 20000 #HACK to remove most of Rayleigh signal
+#        masl = 20000 #HACK to remove most of Rayleigh signal (requires special GRASP build)
         nowPix = pixel(dtNm, 1, 1, lon, lat, masl, lndPrct)
         sza = measData[0]['solar_zenith'][ind] # assume instantaneous measurement
         for l,wl in enumerate(wvls): # LOOP OVER WAVELENGTHS
              phi = measData[l]['solar_azimuth'][ind] - measData[l]['sensor_azimuth'][ind,:] # HINT: might cause phi<-180 which GRASP technically doesn't like
              nbvm = phi.shape[0] 
+             if graspInputs.upper()=='Ionly':
+                 msTyp = np.r_[41]
+                 msrmnts = np.r_[measData[l]['I'][ind,:]]
+             elif graspInputs.upper()=='DOLP':
+                 msTyp = np.r_[41, 44]
+                 msrmnts = np.r_[measData[l]['I'][ind,:], measData[l]['DOLP'][ind,:]]
+             elif graspInputs.upper()=='IQU':
+                 msTyp = np.r_[41, 42, 43]
+                 msrmnts = np.r_[measData[l]['I'][ind,:], measData[l]['Q'][ind,:], measData[l]['U'][ind,:]]
+             else:
+                 assert False, '%s is unrecognized value for graspInputs [Ionly,DOLP,IQU]' % graspInputs
+             msrmnts[np.abs(msrmnts) < GRASP_MIN] = GRASP_MIN # HINT: could change Q or U sign but still small absolute shift
+             nip = msTyp.shape[0]
              phi = np.tile(phi, nip) # ex. 11, 35, 55, 11, 35, 55...
              thtv = np.abs(np.tile(measData[l]['sensor_zenith'], nip))
-#             msrmnts = np.r_[measData[l]['I'][ind,:], measData[l]['DOLP'][ind,:]]
-             msrmnts = np.r_[measData[l]['I'][ind,:], measData[l]['Q'][ind,:], measData[l]['U'][ind,:]]
-             msrmnts[np.abs(msrmnts) < GRASP_MIN] = GRASP_MIN # HINT: could change sign but doesn't matter for DoLP
              nowPix.addMeas(wl, msTyp, np.repeat(nbvm, nip), sza, thtv, phi, msrmnts)
         gObj.addPix(nowPix)
     graspObjs.append(gObj)
 
+
 # Write SDATA, run GRASP and read in results
-graspObjs[0].writeSDATA()
-#graspObjs[0].runGRASP()
-#rslts = graspObjs[0].readOutput()
+gDB = graspDB(graspObjs)
+gDB.processData(maxCPUs, binPathGRASP, savePath)
 
 # Read in model "truth" from levC lidar file
-varNames = ['reff', 'refi', 'refr', 'ssa', 'tau']
-Nwvlth = len(wvlsLidar)
-trueData = [{} for _ in range(Nwvlth)]
-warnings.simplefilter('ignore') # ignore missing_value not cast warning
-for i,wvl in enumerate(wvlsLidar):
-    lidarFN = lidarFNfrmtStr % int(wvl*1000)
-    netCDFobj = Dataset(lidarFN)
-    for varName in varNames:
-        trueData[i][varName] = np.array(netCDFobj.variables[varName])      
-    netCDFobj.close()
-warnings.simplefilter('always')
-for i in range(Nwvlth): 
-    for varName in varNames:
-        trueData[i][varName] = np.delete(trueData[i][varName], invldInd, axis=0)
-    tauKrnl = trueData[i]['tau']
-    trueData[i]['tau'] = np.sum(trueData[i]['tau'], axis=1)
-    tauKrnl = tauKrnl/trueData[i]['tau'].reshape(Npix,1)
-    for varName in np.setdiff1d(varNames, 'tau'):
-        trueData[i][varName] = np.sum(tauKrnl*trueData[i][varName], axis=1)
-
-warnings.simplefilter('ignore') # ignore missing_value not cast warning
-netCDFobj = Dataset(lndCvrFN)
-trueData[0]['BPDFcoef'] = np.array(netCDFobj.variables['BPDFcoef'])
-netCDFobj.close()
-warnings.simplefilter('always')
-trueData[0]['BPDFcoef'] = np.delete(trueData[0]['BPDFcoef'], invldInd)
+#varNames = ['reff', 'refi', 'refr', 'ssa', 'tau']
+#Nwvlth = len(wvlsLidar)
+#trueData = [{} for _ in range(Nwvlth)]
+#warnings.simplefilter('ignore') # ignore missing_value not cast warning
+#for i,wvl in enumerate(wvlsLidar):
+#    lidarFN = lidarFNfrmtStr % int(wvl*1000)
+#    netCDFobj = Dataset(lidarFN)
+#    for varName in varNames:
+#        trueData[i][varName] = np.array(netCDFobj.variables[varName])      
+#    netCDFobj.close()
+#warnings.simplefilter('always')
+#for i in range(Nwvlth): 
+#    for varName in varNames:
+#        trueData[i][varName] = np.delete(trueData[i][varName], invldInd, axis=0)
+#    tauKrnl = trueData[i]['tau']
+#    trueData[i]['tau'] = np.sum(trueData[i]['tau'], axis=1)
+#    tauKrnl = tauKrnl/trueData[i]['tau'].reshape(Npix,1)
+#    for varName in np.setdiff1d(varNames, 'tau'):
+#        trueData[i][varName] = np.sum(tauKrnl*trueData[i][varName], axis=1)
+#
+#warnings.simplefilter('ignore') # ignore missing_value not cast warning
+#netCDFobj = Dataset(lndCvrFN)
+#trueData[0]['BPDFcoef'] = np.array(netCDFobj.variables['BPDFcoef'])
+#netCDFobj.close()
+#warnings.simplefilter('always')
+#trueData[0]['BPDFcoef'] = np.delete(trueData[0]['BPDFcoef'], invldInd)
--- GRASPfromNetCDF_plots.py
+++ GRASPfromNetCDF_plots.py
@@ -2,41 +2,62 @@
 # -*- coding: utf-8 -*-
 
 import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+sys.path.append(os.path.join("..", "GRASP_PythonUtils"))
+from runGRASP import graspDB
 
-#pltInd = 94 # pixel index to plot (especially bad one)
-pltInd = 48 # pixel index to plot
-polVar = 'PoI' #'P' or 'PoI'
-#titleStr = 'BRDF + Rayleigh'
-#titleStr = 'Rayleigh Only'
+pltInd = 4 # pixel index to plot
+polVar = 'PoI' #'P' (absolute) or 'PoI' (relative)
 titleStr = 'BRDF + BPDF + Rayleigh'
+rsltsFile = '/Users/wrespino/Desktop/MADCAP_test.pkl'
 
-outFile = '/Users/wrespino/Synced/Remote_Sensing_Projects/MADCAP_CAPER/graspWorking/harp_inversionRslts_16bin_looseControl.txt'
-#outFile = '/Users/wrespino/Synced/Remote_Sensing_Projects/MADCAP_CAPER/graspWorking/harp_inversionRslts_16bin_looseSlwRT.txt'
-#outFile = '/Users/wrespino/Synced/Remote_Sensing_Projects/MADCAP_CAPER/graspWorking/harp_inversionRslts_LN_bestContol.txt'
-#outFile = '/Users/wrespino/Synced/Remote_Sensing_Projects/MADCAP_CAPER/graspWorking/harp_inversionRslts_LN_looseContol.txt'
-#outFile = '/Users/wrespino/Synced/Remote_Sensing_Projects/MADCAP_CAPER/graspWorking/harp_inversionRslts_LN_looseContol_1RI.txt'
-#outFile = '/Users/wrespino/Synced/Remote_Sensing_Projects/MADCAP_CAPER/graspWorking/harp_inversionRslts_LN_looseslwRT.txt'
+yVarNm = 'n'
+xVarNm = 'k'
+yInd = [0]
+xInd = [0]
+#cVarNm = 'vol'
+cVarNm= False
+cInd = [0]
 
-rslts = graspObjs[0].readOutput(outFile)
+filterSites = False
+#frbdnSites = np.r_[179,1146,535,840,175] # need to uncomment/comment below to activate
+keepSites = np.r_[9] #9,518,77,1,285,514,946,961
+
+
+gDB = graspDB()
+gDB.loadResults(rsltsFile)
+
+gDB = graspDB()
+gDB.loadResults(rsltsFile)
+pltRslt = gDB.rslts[pltInd]
 
 # PLOTTING CODE
-Nwvlth = 1 if np.isscalar(rslts[pltInd]['lambda']) else len(rslts[pltInd]['lambda'])
+Nwvlth = 1 if np.isscalar(pltRslt['lambda']) else len(pltRslt['lambda'])
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
-objs = ax[0].plot(rslts[pltInd]['sca_ang'], rslts[pltInd]['fit_I']) #, linestyle='none')
+objs = ax[0].plot(pltRslt['sca_ang'], pltRslt['fit_I']) #, linestyle='none')
 clrMat = np.tile([obj.get_color() for obj in objs], Nwvlth)
-ax[0].scatter(rslts[pltInd]['sca_ang'], rslts[pltInd]['meas_I'], c=clrMat, marker='x')
+ax[0].scatter(pltRslt['sca_ang'], pltRslt['meas_I'], c=clrMat, marker='x')
 ax[0].set_xlabel("Scattering Angle (deg)")
 ax[0].set_ylabel("I")
 if Nwvlth>1:
-    ax[0].legend(['%4.2f μm' % l for l in rslts[pltInd]['lambda']],ncol=2, prop={'size': 12}, loc = 'upper left')
+    ax[0].legend(['%4.2f μm' % l for l in pltRslt['lambda']],ncol=2, prop={'size': 12}, loc = 'upper left')
 else:
     ax[0].legend(['GRASP', 'OSSE'])
-    plt.suptitle(titleStr + ' (%4.2f μm)' % rslts[pltInd]['lambda'])
-objs = ax[1].plot(rslts[pltInd]['sca_ang'], rslts[pltInd]['fit_'+polVar]) #, linestyle='none')
-ax[1].scatter(rslts[pltInd]['sca_ang'], rslts[pltInd]['meas_'+polVar], c=clrMat, marker='x')
+    plt.suptitle(titleStr + ' (%4.2f μm)' % pltRslt['lambda'])
+objs = ax[1].plot(pltRslt['sca_ang'], pltRslt['fit_'+polVar]) #, linestyle='none')
+ax[1].scatter(pltRslt['sca_ang'], pltRslt['meas_'+polVar], c=clrMat, marker='x')
 ax[1].set_xlabel("Scattering Angle (deg)")
 ax[1].set_ylabel("DoLP")
 plt.tight_layout()
+
+vldInd = slice(None)
+# MANUAL SCAT PLOT
+plt.figure(figsize=(6,5))
+gDB.scatterPlot(xVarNm, yVarNm, 1, 1, cVarNm, 1, one2oneScale=False, 
+                    logScl=False, customAx=plt.gca(), Rstats=False, rsltInds=vldInd, pltLabel=os.path.basename(rsltsFile))
+
 
 #minVal = np.array([])
 #minValFit = np.array([])
