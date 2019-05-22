fixing small plotting bug, change PSD computation from netCDF, adding 2lambda and LIDAR YAMLs

GRASPfromNetCDF_hemisphereCompare.py
@@ -15,16 +15,16 @@ import scipy.ndimage as ndimage
 # Paths to files
 basePath = '/Users/wrespino/Synced/' # NASA MacBook
 rmtPrjctPath = os.path.join(basePath, 'Remote_Sensing_Projects/MADCAP_CAPER/VLIDORTbench_graspConfig_12')
-radianceFNfrmtStr = os.path.join(rmtPrjctPath, 'benchmark_rayleigh+aerosol_BRDF/calipso-g5nr.vlidort.vector.MODIS_BRDF.%dd00.nc4')
-rsltsFile = findNewestMatch(os.path.split(radianceFNfrmtStr)[0], pattern='aerosol_bench*.pkl')
+radianceFNfrmtStr = os.path.join(rmtPrjctPath, 'benchmark_rayleigh+aerosol_nosurface/calipso-g5nr.vlidort.vector.LAMBERTIAN.%dd00.nc4')
+rsltsFile = findNewestMatch(os.path.split(radianceFNfrmtStr)[0], pattern='noSruf_bench_OneHexQuadExpnd*3ae92d3d.pkl')
 savePlotPath = os.path.split(radianceFNfrmtStr)[0]
 
 #varNames = ['I', 'Q', 'U', 'surf_reflectance', 'surf_reflectance_Q', 'surf_reflectance_U', 'sensor_zenith', 'sensor_azimuth']
 varNames = ['I', 'Q', 'U', 'Q_scatplane', 'U_scatplane', 'surf_reflectance', 'surf_reflectance_Q', 'surf_reflectance_U', 'surf_reflectance_Q_scatplane','surf_reflectance_U_scatplane', 'sensor_zenith', 'sensor_azimuth']
 
-pltVar = 'I'
+pltVar = 'DOLP'
 noRayleigh = False # only compare with surface reflectance
-relDeltaI = True # relative (True) or absolute (False) I/Q/U difference (no effect on DOLP)
+relDeltaI = False # relative (True) or absolute (False) I/Q/U difference (no effect on DOLP)
 
 print('Loading results from: ', os.path.split(rsltsFile)[1])
 gDB = graspDB()
@@ -50,7 +50,7 @@ measData = readVILDORTnetCDF(varNames, radianceFNfrmtStr, wvls)
 Nwvl = wvls.shape[0]
 
 fig, ax = plt.subplots(Nwvl, 3, subplot_kw=dict(projection='polar'), figsize=(14,3+3*Nwvl))
-if Nwvl == 1: ax = ax[:,None]
+if Nwvl == 1: ax = ax[None,:]
 #else:
 #    fig, ax = plt.subplots(3,Nwvl, subplot_kw=dict(projection='polar'), figsize=(20,15))
 for l in range(Nwvl):

netCDF2YAML.py
@@ -12,15 +12,15 @@ from scipy import interpolate as intrp
 
 # Paths to files
 basePath = '/Users/wrespino/Synced/' # NASA MacBook
-radianceFNfrmtStr = os.path.join(basePath, 'Remote_Sensing_Projects/MADCAP_CAPER/VLIDORTbench_graspConfig_12/benchmark_rayleigh+aerosol_BRDF/calipso-g5nr.vlidort.vector.MODIS_BRDF.%dd00.nc4')
+radianceFNfrmtStr = os.path.join(basePath, 'Remote_Sensing_Projects/MADCAP_CAPER/VLIDORTbench_graspConfig_12/benchmark_rayleigh+aerosol_nosurface/calipso-g5nr.vlidort.vector.LAMBERTIAN.%dd00.nc4')
 
-rsltsFile = os.path.join(basePath, 'Remote_Sensing_Projects/MADCAP_CAPER/sulfateBenchmark/sulfate_bench_fit__PSDlt600nm_BRDF_FREE.pkl')
+rsltsFile = os.path.join(basePath, 'Remote_Sensing_Projects/MADCAP_CAPER/VLIDORTbench_graspConfig_12/benchmark_rayleigh+aerosol_nosurface/noSruf_bench_OneHexQuadExpnd_865nm_YAML216cbfed.pkl')
 
 varNames = ['I', 'Q', 'U', 'sensor_zenith', 'RGEO', 'RISO', 'RVOL', 'TAU', 'VOL', 'radius', 'TOTdist', 'REFR', 'REFI', 'SSA', 'ZE', 'U10m', 'V10m', 'ROT', 'SUdist', 'AREA', 'REFF']
 #wvls = [0.410, 0.440, 0.550, 0.670, 1.020, 2.100] # wavelengths to read from levC files
-wvls = [0.470, 0.865] # wavelengths to read from levC files
+wvls = [0.865] # wavelengths to read from levC files
 
-extensiveVars = ['TAU', 'VOL', 'TOTdist', 'ROT', 'SUdist', 'AREA']
+extensiveVars = ['TAU', 'TOTdist', 'ROT', 'SUdist', 'AREA']
 
 intrpRadii = np.array([0.05, 0.059009, 0.06964, 0.082188, 0.096996, 0.11447, 0.1351, 0.15944, 0.18816, 0.22206, 0.26207, 0.30929, 0.36502, 0.43078, 0.5084, 0.6])
 
@@ -33,11 +33,11 @@ measData = readVILDORTnetCDF(varNames, radianceFNfrmtStr, wvls)
 # Read in model "truth" from levC lidar file
 Nwvlth = len(wvls)
 Nlayer = measData[0]['ZE'].shape[0]-1
-volInt = np.sum(measData[0]['VOL']*np.diff(-measData[0]['ZE'])) # column concentration (m^3/m^2)
 for i in range(Nwvlth):  # integrate over all layers
     tauKrnl = measData[i]['TAU']/np.sum(measData[i]['TAU']) # weight intensive parameters by layer optical depth
     measData[i]['ZE_edge'] = measData[i]['ZE']
     measData[i]['ZE'] = (measData[i]['ZE'][0:-1]+measData[i]['ZE'][1:])/2 # ZE now midpoint, ZE_all is AOD weighted average height 
+    measData[i]['TOT_COL_dvdlnr'] = np.sum(measData[i]['TOTdist'].T*np.diff(-measData[i]['ZE_edge']), axis=1) # dv/dlnr (um^3/um^2)
     for varName in list(measData[i]):           
         if varName in extensiveVars:
             measData[i][varName+'_all'] = np.sum(measData[i][varName], axis=0)
@@ -46,9 +46,6 @@ for i in range(Nwvlth):  # integrate over all layers
             measData[i][varName+'_all'] = np.sum(np.tile(tauKrnl,(Nflds,1)).T*measData[i][varName], axis=0)
         elif not np.isscalar(measData[i][varName]) and measData[i][varName].shape[0]==Nlayer:
             measData[i][varName+'_all'] = np.sum(tauKrnl*measData[i][varName])
-    TOTdistIntegral = np.trapz(measData[i]['TOTdist_all'], x=np.log(measData[i]['radius'])) 
-    measData[i]['TOT_COL_dvdlnr'] = volInt/TOTdistIntegral*measData[i]['TOTdist_all'] # dv/dlnr (m^3/m^2)
-
 
 #print('<> Wavelengths (LAMBDA):')
 #print(wvls)
@@ -65,18 +62,18 @@ for i in range(Nwvlth):  # integrate over all layers
 #    print(np.array([md['U10m'] for md in measData]).tolist());
 #    print((np.array([md['V10m'] for md in measData])).tolist());
 #    
-#if 'SSA' in measData[0]:
-#    print('<> Spectral Aerosol Parameters (RRI;IRI;SSA;TAU):')
-#    print(np.array([md['REFR_all'] for md in measData]).tolist());
-#    print(np.array([md['REFI_all'] for md in measData]).tolist());
-#    print(np.array([md['SSA_all'] for md in measData]).tolist());
-#    print(np.array([md['TAU_all'] for md in measData]).tolist());
+if 'SSA' in measData[0]:
+    print('<> Spectral Aerosol Parameters (RRI;IRI;SSA;TAU):')
+    print(np.array([md['REFR_all'] for md in measData]).tolist());
+    print(np.array([md['REFI_all'] for md in measData]).tolist());
+    print(np.array([md['SSA_all'] for md in measData]).tolist());
+    print(np.array([md['TAU_all'] for md in measData]).tolist());
 #
 
 intrpRadii = np.logspace(np.log10(0.05),np.log10(0.5),40) # this will match GRASP to 8 digits
 Nradii = len(intrpRadii)
 if 'radius' in measData[0]:
-    dvdlnr = intrp.interp1d(measData[0]['radius'],measData[0]['TOT_COL_dvdlnr']*1e6) #dv/dlnr (um^3/um^2)
+    dvdlnr = intrp.interp1d(measData[0]['radius'],measData[0]['TOT_COL_dvdlnr']) #dv/dlnr (um^3/um^2)
     print('<> Size Distribution (Radii;dvdlnr;min;max;wvlngInvlv):')
     print(intrpRadii.tolist())
     print(np.maximum(dvdlnr(intrpRadii),1.1e-13).tolist())
@@ -84,12 +81,31 @@ if 'radius' in measData[0]:
     print((5.0*np.ones(Nradii)).tolist())
     print((np.zeros(Nradii,dtype=int)).tolist())
 #
-#print('<> Rayleigh optical depth (ROT):')
-#print((np.array([md['ROT_all'] for md in measData])).tolist());
-#
-#print('<> AOD weighted mean height [m]:')
-#print((np.array([md['ZE_all'] for md in measData])).tolist());
+print('<> Rayleigh optical depth (ROT):')
+print((np.array([md['ROT_all'] for md in measData])).tolist());
 
+print('<> AOD weighted mean height [m]:')
+print((np.array([md['ZE_all'] for md in measData])).tolist());
+
+
+
+plt.figure()
+plt.plot(measData[0]['radius'],measData[0]['TOT_COL_dvdlnr'],gDB.rslts[0]['r'],gDB.rslts[0]['vol']*gDB.rslts[0]['dVdlnr'],'x')
+plt.xlim(0.04,0.6)
+plt.xscale('log')
+plt.xlabel('radius (μm)')
+plt.ylabel('dv/dlnr ($μm^3/μm^3$)')
+plt.legend(['netCDF', 'GRASP'])
+
+#totPSD = np.zeros(measData[0]['TOTdist'].shape[1])
+#for h in range(measData[0]['VOL'].shape[0]):
+#    TOTdistIntegral = np.trapz(measData[0]['TOTdist'][h,:], x=np.log(measData[0]['radius']))
+#    nrmFactor = measData[0]['VOL'][h]/TOTdistIntegral*np.diff(-measData[0]['ZE_edge'])[h]*1e6
+#    totPSD = totPSD + nrmFactor*measData[0]['TOTdist'][h,:]
+#plt.plot(measData[0]['radius'],totPSD)
+
+
+#plt.plot(measData[0]['radius'],gDB.rslts[0]['dVdlnr'].max()/measData[0]['TOTdist'].max()*measData[0]['TOTdist'].T)
 
 # [outdated] sanity checks
 #ind = 63            
@@ -121,3 +137,4 @@ if 'radius' in measData[0]:
 #plt.plot(measData[l]['radius'], measData[l]['TOTdist']/measData[l]['TOTdist'].max())
 #plt.plot(gDB.rslts[t]['r'], gDB.rslts[t]['dVdlnr']/gDB.rslts[t]['dVdlnr'].max())
 #plt.xscale('log')
+