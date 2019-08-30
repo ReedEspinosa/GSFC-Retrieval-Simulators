f434170c35d77b624b991a294d045b8dc55f06e2
Reed Espinosa <reed.espinosa@nasa.gov>
2019-08-30 12:48:35 -0400

    working prototype of simulateRetrieval.py

--- simulateRetrieval.py
+++ simulateRetrieval.py
@@ -9,31 +9,66 @@ Created on Wed Jun  5 17:40:41 2019
 import numpy as np
 import os
 import sys
+import copy
 sys.path.append(os.path.join("..", "GRASP_scripts"))
 import pickle
-from runGRASP import graspDB, graspRun, pixel
+from runGRASP import graspRun, pixel
 
-fwdModelYAMLpath = '/Users/wrespino/Synced/Remote_Sensing_Projects/MADCAP_CAPER/newOpticsTables/settings_optics_SU.v1_5_gpmom_sizedist.yml'
-fwdTruthSavePath = '/Users/wrespino/Desktop/testFwd.pkl'
+fwdModelYAMLpath = '/Users/wrespino/Synced/Remote_Sensing_Projects/A-CCP/canonical_cases/settings_FWD_IQU_5lambda_Template.yml'
+bckYAMLpath = '/Users/wrespino/Synced/Remote_Sensing_Projects/A-CCP/canonical_cases/settings_BCK_IQU_5lambda_Template.yml'
+savePath = '/Users/wrespino/Desktop/testFwd_SPH90.pkl'
+Nsims = 30
 
-# FAKE MEASUREMENTS (determined by architecture)
+# DUMMY MEASUREMENTS (determined by architecture, should ultimatly move to seperate scripts)
+#  For more than one measurement type or viewing geometry pass msTyp, nbvm, thtv, phi and msrments as vectors: \n\
+#  len(msrments)=len(thtv)=len(phi)=sum(nbvm); len(msTyp)=len(nbvm) \n\
+#  msrments=[meas[msTyp[0],thtv[0],phi[0]], meas[msTyp[0],thtv[1],phi[1]],...,meas[msTyp[0],thtv[nbvm[0]],phi[nbvm[0]]],meas[msTyp[1],thtv[nbvm[0]+1],phi[nbvm[0]+1]],...]'
+measNm = ['I', 'Q', 'U'] # should match GRASP output (e.g. fit_I -> 'I')
+msTyp = [41, 42, 43]
+nbvm = [9, 9, 9]
+sza = 30
+thtv = np.tile([70.5, 60.0, 45.6, 26.1, 0, 26.1, 45.6, 60.0, 70.5], len(nbvm))
+phi = np.tile([0, 0, 0, 0, 0, 180, 180, 180, 180], len(nbvm))
+meas = np.r_[np.repeat(0.1, nbvm[0]), np.repeat(0.01, nbvm[1]), np.repeat(0.01, nbvm[2])] 
 nowPix = pixel(730123.0, 1, 1, 0, 0, 0, 100)
-#nowPix.addMeas(0.350, [41], 1, 30, [0, 0, 10, 20], [0, 10, 10, 10], np.repeat(0.123,4)) # wl, msTyp, nbvm, sza, thtv, phi, msrmnts
-nowPix.addMeas(0.350, [41], 2, 30, [0, 10], [0, 0], np.repeat(0.123,2)) # wl, msTyp, nbvm, sza, thtv, phi, msrmnts
-nowPix.addMeas(0.50, [41], 2, 30, [0, 10], [0, 0], np.repeat(0.123,2))
-nowPix.addMeas(0.70, [41], 2, 30, [0, 10], [0, 0], np.repeat(0.123,2))
-nowPix.addMeas(1.0, [41], 2, 30, [0, 10], [0, 0], np.repeat(0.123,2))
-nowPix.addMeas(1.5, [41], 2, 30, [0, 10], [0, 0], np.repeat(0.123,2))
+nowPix.addMeas(0.350, msTyp, nbvm, sza, thtv, phi, meas)
+nowPix.addMeas(0.50, msTyp, nbvm, sza, thtv, phi, meas)
+nowPix.addMeas(0.70, msTyp, nbvm, sza, thtv, phi, meas)
+nowPix.addMeas(1.0, msTyp, nbvm, sza, thtv, phi, meas)
+nowPix.addMeas(1.5, msTyp, nbvm, sza, thtv, phi, meas)
+nbvm = np.tile(nbvm, [len(nowPix.measVals),1]) # this assumes nbvm(wvlth,msType) is same at all lambda
+measNm = np.tile(measNm, [len(nowPix.measVals),1]) # this assumes nbvm(wvlth,msType) is same at all lambda
 
-# THIS COULD PROBABLY MOVE TO A FUNCTION WITHIN runGRASP
+def addError(meas, measNm):
+    if measNm=='I':
+        return meas*(1+np.random.normal()*0.03)
+    elif measNm=='Q' or measNm=='U':
+        return meas*(1+np.random.normal()*0.005)
+    else:
+        assert False, 'Unkown measurement string, can not add error!'
+
+# RUN THE FOWARD MODEL
 gObjFwd = graspRun(fwdModelYAMLpath)
 gObjFwd.addPix(nowPix)
-gObjFwd.writeSDATA()
 gObjFwd.runGRASP()
 rsltFwd = gObjFwd.readOutput()
-with open(fwdTruthSavePath, 'wb') as f:
-    pickle.dump(rsltFwd, f, pickle.HIGHEST_PROTOCOL)
 
+# ADD NOISE AND PERFORM RETRIEVALS
+gObjBck = graspRun(bckYAMLpath)
+for i in range(Nsims):
+    nowPix.dtNm = copy.copy(nowPix.dtNm)
+    for l, msDct in enumerate(nowPix.measVals):
+        edgInd = np.r_[0, np.cumsum(nbvm[l,:])]
+        msDct['measurements'] = copy.copy(msDct['measurements']) # we are going to write to this
+        for i in range(len(nbvm[l,:])):
+            fwdSim = rsltFwd[0]['fit_'+measNm[l,i]][:,l]
+            fwdSim = copy.copy(addError(fwdSim,measNm[l,i]))
+            msDct['measurements'][edgInd[i]:edgInd[i+1]] = fwdSim
+    nowPix.dtNm = nowPix.dtNm+1 # otherwise GRASP will whine
+    gObjBck.addPix(nowPix)
+gObjBck.runGRASP()
+rsltBck = gObjBck.readOutput()
 
-
-
+rsltBck.append(rsltFwd[0]) # the last element in the list corresponds to the "truth"
+with open(savePath, 'wb') as f:
+    pickle.dump(rsltBck, f, pickle.HIGHEST_PROTOCOL)
