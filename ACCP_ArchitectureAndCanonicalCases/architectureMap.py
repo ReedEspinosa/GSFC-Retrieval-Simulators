8de56b9ae076760c5991f440e60b7e6d8c920018
Reed Espinosa <reed.espinosa@nasa.gov>
2019-09-06 13:38:22 -0400

    creating ACCP architectureMap

---
+++ ACCP_ArchitectureAndCanonicalCases/architectureMap.py
@@ -0,0 +1,63 @@
+import numpy as np
+import os
+import sys
+sys.path.append(os.path.join(".."))
+import runGRASP as rg
+import functools        
+
+# DUMMY MEASUREMENTS (determined by architecture, should ultimatly move to seperate scripts)
+#  For more than one measurement type or viewing geometry pass msTyp, nbvm, thtv, phi and msrments as vectors: \n\
+#  len(msrments)=len(thtv)=len(phi)=sum(nbvm); len(msTyp)=len(nbvm) \n\
+#  msrments=[meas[msTyp[0],thtv[0],phi[0]], meas[msTyp[0],thtv[1],phi[1]],...,meas[msTyp[0],thtv[nbvm[0]],phi[nbvm[0]]],meas[msTyp[1],thtv[nbvm[0]+1],phi[nbvm[0]+1]],...]'
+def returnPixel(archName, sza=30, landPrct=100, relPhi=0, nowPix=None): 
+    if not nowPix: nowPix = rg.pixel(730123.0, 1, 1, 0, 0, 0, landPrct)
+    assert nowPix.land_prct == landPrct, 'landPrct provided did not match land percentage in nowPix'
+    if archName is 'polar07': # CURRENTLY ONLY USING JUST 10 ANGLES IN RED
+        msTyp = [41, 42, 43] # must be in ascending order
+        nbvm = 10*np.ones(len(msTyp), np.int)
+        thtv = np.tile([-57.0,  -44.0,  -32.0 ,  -19.0 ,  -6.0 ,  6.0,  19.0,  32.0,  44.0,  57.0], len(msTyp))
+        wvls = [0.360, 0.380, 0.410, 0.550, 0.670, 0.870, 1.650] # Nλ=7
+        meas = np.r_[np.repeat(0.1, nbvm[0]), np.repeat(0.01, nbvm[1]), np.repeat(0.01, nbvm[2])] 
+        phi = np.array([relPhi if tv>0 else relPhi+180 for tv in thtv]) # currently we assume all observations fall within a plane
+        for wvl in wvls: # This will be expanded for wavelength dependent measurement types/geometry
+            nowPix.addMeas(wvl, msTyp, nbvm, sza, thtv, phi, meas)
+            nowPix.measVals[-1]['errorModel'] = functools.partial(addError, 'basePolar-7') # this must link to an error model in addError() below
+    return nowPix
+
+def addError(measNm, l, rsltFwd, edgInd):
+    # if the following check ever becomes a problem see commit #6793cf7
+    assert (np.diff(edgInd)[0]==np.diff(edgInd)).all(), 'Current error models assume that each measurement type has the same number of measurements at each wavelength!'
+    if measNm.split('-')[0] == 'basePolar': # measNm should be string w/ format 'basePolar-N', where N is polarimeter number
+        if measNm.split('-')[1] in ['4', '7', '8']: # S-Polar04 (a-d), S-Polar07, S-Polar08
+            relErr = 0.03
+            relDoLPErr = 0.005
+        elif measNm.split('-')[1] in ['1', '2', '3']: # S-Polar01, S-Polar2 (a-b), S-Polar3 [1st two state ΔI as "4% to 6%" in RFI]
+            relErr = 0.05
+            relDoLPErr = 0.005
+        elif measNm.split('-')[1] in ['9']: # S-Polar09
+            relErr = 0.03
+            relDoLPErr = 0.003
+        elif measNm.split('-')[1] in ['5']: # S-Polar05
+            relErr = 0.02
+            relDoLPErr = 0.003
+        else:
+            assert False, 'No error model found for %s!' % measNm # S-Polar06 has DoLP dependent ΔDoLP
+        trueSimI = rsltFwd['fit_I'][:,l]
+        trueSimQ = rsltFwd['fit_Q'][:,l]
+        trueSimU = rsltFwd['fit_U'][:,l]
+        noiseVctI = np.random.lognormal(sigma=np.log(1+relErr), size=len(trueSimI))
+        fwdSimI = trueSimI*noiseVctI
+        fwdSimQ = trueSimQ*noiseVctI # we scale Q and U too to keep q, u and DoLP inline with truth
+        fwdSimU = trueSimU*noiseVctI
+        dpRnd = np.random.normal(size=len(trueSimI))*relDoLPErr
+        dPol = dpRnd*trueSimI*np.sqrt((trueSimQ**2+trueSimU**2)/(trueSimQ**4+trueSimU**4)) # true is fine b/c noiceVctI factors cancel themselves out
+        fwdSimQ = fwdSimQ*(1+dPol)
+        fwdSimU = fwdSimU*(1+dPol) # Q and U errors are 100% correlated here
+        fwdSim = np.r_[fwdSimI, fwdSimQ, fwdSimU] # safe because of ascending order check in simulateRetrieval.py
+    return fwdSim
+
+
+
+
+
+        
