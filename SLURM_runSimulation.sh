b667dd8ee74218acd0819432df16bf13618ca39e
Reed Espinosa <reed.espinosa@nasa.gov>
2020-02-04 14:34:41 -0500

    updates for SPA, including *old3Lambda_2mode.yml that was used

--- SLURM_runSimulation.sh
+++ SLURM_runSimulation.sh
@@ -1,10 +1,10 @@
 #!/usr/local/bin/bash
-#SBATCH --job-name=ACCP_1
+#SBATCH --job-name=ACCP_SPA
 #SBATCH --nodes=1 --constraint=hasw
 #SBATCH --time=2:00:00
 #SBATCH -o log/output.%A-%a
 #SBATCH -e log/error.%A-%a
-#SBATCH --array=0,2,6,8
+#SBATCH --array=0-129
 
 date
 hostname
