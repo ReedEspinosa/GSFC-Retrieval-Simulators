36bae5cdcf5efadce6a9ec6aa0a88a770a379f11
Reed Espinosa <reed.espinosa@nasa.gov>
2020-02-19 16:06:16 -0500

    AERONET_PDF_Plot settings used for plots presented to MADCAP; plotSimResutls_Fits is slightly more flexible (can handle only HSRL α or β)

--- plotSimResults_Fits.py
+++ plotSimResults_Fits.py
@@ -4,14 +4,17 @@
 
 import numpy as np
 import os
+import sys
+MADCAPparentDir = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) # we assume GRASP_scripts is in parent of MADCAP_scripts
+sys.path.append(os.path.join(MADCAPparentDir, "GRASP_scripts"))
 from simulateRetrieval import simulation
 from miscFunctions import matplotlibX11
 matplotlibX11()
 import matplotlib.pyplot as plt
 
-simRsltFile = '/Users/wrespino/synced/Working/SIM14_lidarPolACCP/SIM43V2_2mode_lidar05+polar07_case-variable_sza30_phi0_tFct0.12_V2.pkl'
+simRsltFile = '/Users/wrespino/synced/Working/SIM14_lidarPolACCP/SIM43V30_2mode_lidar05+polar07_case-case06c_sza30_phi0_tFct1.00_V2.pkl'
 #simRsltFile = '/Users/wrespino/Synced/Working/SIM13_lidarTest/SIM43_lidar05+polar07_case-case06cmonomode_sza30_phi0_tFct1.00_V2.pkl'
-lIndL = 3 # LIDAR λ to plot
+lIndL = 3 # LIDAR λ to plot (3,7)
 lIndP = 4 # polarimeter λ to plot
 
 simA = simulation(picklePath=simRsltFile)
@@ -27,8 +30,7 @@ color1 = np.array([
 rngVar = 'RangeLidar'
 profExtNm = 'βext'
 βfun = lambda i,l,d: d['aodMode'][i,l]*d[profExtNm][i,:]/np.mean(d[profExtNm][i,:])
-hsrl = 'fit_VExt' in simA.rsltFwd
-measTypesL = ['VExt', 'VBS'] if hsrl else ['LS']
+measTypesL = [x for x in ['VExt', 'VBS', 'LS'] if 'fit_'+x in simA.rsltFwd]
 assert not np.isnan(simA.rsltFwd['fit_'+measTypesL[0]][0,lIndL]), 'Nans found in LIDAR data at this wavelength! Is the value of lIndL valid?'
 figL, axL = plt.subplots(1,len(measTypesL)+1,figsize=(12,6))
 # Polar Prep
@@ -40,6 +42,7 @@ figP, axP = plt.subplots(1,len(measTypesP),figsize=(12,6))
 # Plot LIDAR and Polar measurements and fits
 NfwdModes = simA.rsltFwd['aodMode'].shape[0]
 NbckModes = simA.rsltBck[0]['aodMode'].shape[0]
+frstPass = True
 for rb in simA.rsltBck:
 #    [np.sum(rb[profExtNm][i,:]*rb['range'][i,:]) for i in range(rb['range'].shape[0])]
 #    botPrf = 0 if np.sum(rb[profExtNm][0,:]*rb['range'][0,:])>np.sum(rb[profExtNm][1,:]*rb['range'][1,:]) else 1
@@ -47,6 +50,8 @@ for rb in simA.rsltBck:
 #    axA[0].plot(βfun((botPrf+1)%2,lInd,rb), rb['range'][(botPrf+1)%2,:]/1e3, color=color2, alpha=alphVal)
     for i in range(NbckModes):
         axL[0].plot(βfun(i,lIndL,rb), rb['range'][i,:]/1e3, color=color1[i], alpha=alphVal)
+#        if frstPass: print(βfun(i,lIndL,rb)[rb['range'][i,:]>2200])
+    frstPass = False
     for i,mt in enumerate(measTypesL): # Lidar retrieval meas & fit
         axL[i+1].plot(1e6*rb['meas_'+mt][:,lIndL], rb[rngVar][:,lIndL]/1e3, color=color1[0], alpha=alphVal)
         axL[i+1].plot(1e6*rb['fit_'+mt][:,lIndL], rb[rngVar][:,lIndL]/1e3, color=color1[1], alpha=alphVal)
@@ -77,19 +82,25 @@ axL[0].legend(list(map(list, zip(*mdHnd)))[0], lgTxt)
 axL[0].set_ylabel('Altitude (km)')
 axL[0].set_xlabel('Modal Extinction (A.U.)')
 axL[0].set_xlim([0,0.7])
-if hsrl:
-    axL[1].set_xlabel('Extinction ($Mm^{-1}$)')
-    axL[2].set_xlabel('Backscatter ($Mm^{-1}Sr^{-1}$)')
-else:
-    axL[1].set_xlabel('Attenuated Backscatter ($Mm^{-1}Sr^{-1}$)')
+for i,mt in enumerate(measTypesL): # loop throuh measurement types to label x-axes
+    if mt == 'VExt':
+        lblStr = 'Extinction ($Mm^{-1}$)'
+    elif mt == 'VBS':
+        lblStr = 'Backscatter ($Mm^{-1}Sr^{-1}$)'
+    elif mt == 'LS':
+        lblStr = 'Attenuated Backscatter ($Mm^{-1}Sr^{-1}$)'
+    else:
+        lblStr = mt
+    axL[i+1].set_xlabel(lblStr)
 fn = os.path.splitext(simRsltFile)[0].split('/')[-1]
 ttlTxt = '%s [%5.3f μm]' % (fn, simA.rsltFwd['lambda'][lIndL])
 figL.suptitle(ttlTxt)
+figL.tight_layout(rect=[0, 0.03, 1, 0.95])
 # touch up Polarimeter plots
 axP[0].set_ylabel('Reflectance')
 ttlTxt = '%s [%5.3f μm]' % (fn, simA.rsltFwd['lambda'][lIndP])
 figP.suptitle(ttlTxt)
-
+figP.tight_layout(rect=[0, 0.03, 1, 0.95])
 
 # For X11 on Discover
 #plt.ioff()
