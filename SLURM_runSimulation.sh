393f88ca6337f27ace3fa49b3bb59e052718f053
Reed Espinosa <reed.espinosa@nasa.gov>
2020-04-17 20:53:00 -0400

    CONCASE02 - double YAML noise for HSRL ext & bck

--- SLURM_runSimulation.sh
+++ SLURM_runSimulation.sh
@@ -4,11 +4,12 @@
 #SBATCH --time=0:19:00
 #SBATCH -o log/output.%A-%a
 #SBATCH -e log/error.%A-%a
-#SBATCH --array=0-180
+#SBATCH --array=0-360:2
 
 date
 hostname
 echo "---Running Sims N="${SLURM_ARRAY_TASK_ID}"+m, for m=0"
 python runRetrievalSimulation.py $((${SLURM_ARRAY_TASK_ID}+0)) &
+python runRetrievalSimulation.py $((${SLURM_ARRAY_TASK_ID}+1)) &
 wait
 exit 0
