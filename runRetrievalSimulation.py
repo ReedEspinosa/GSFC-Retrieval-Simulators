02eded977ac589b4d2a2d81c44b44c4c3e04fe0d
Reed Espinosa <reed.espinosa@nasa.gov>
2020-05-26 23:18:49 -0400

    CONCASE4MODEV09 - full simulation, with default then random initial guesses
    0<n<48 correspond to the YAML default initial guess
    48<=n<96 have random initial guesses (28 random sets for 56 retrievals)
    This is file that seemed to converge the most in no noise case6d with lidar 06
    Main change is update to scale for fintie differences

--- runRetrievalSimulation.py
+++ runRetrievalSimulation.py
@@ -15,20 +15,19 @@ from architectureMap import returnPixel
 from canonicalCaseMap import setupConCaseYAML
 
 n = int(sys.argv[1]) # (0,1,2,...,N-1)
-# n=0
 
 dryRun = False # set everything up but don't actually retrieve (probably used with fullSave=True)
 fullSave = True # archive all the GRASP working directories into a zip file saved along side the pkl file 
 
 if checkDiscover(): # DISCOVER
     basePath = os.environ['NOBACKUP']
-    saveStart = os.path.join(basePath, 'synced/Working/SIM15_pre613SeminarApr2020/CONCASE4MODEV08_n%d_' % n)
+    saveStart = os.path.join(basePath, 'synced/Working/SIM15_pre613SeminarApr2020/CONCASE4MODEV09_n%d_' % n)
     ymlDir = os.path.join(basePath, 'MADCAP_scripts/ACCP_ArchitectureAndCanonicalCases/')
     dirGRASP = os.path.join(basePath, 'grasp_open/build/bin/grasp')
     krnlPath = os.path.join(basePath, 'local/share/grasp/kernels')
 #     Nsims = 1
 #     maxCPU = 1
-    Nsims = 28
+    Nsims = 56
     maxCPU = 28
 else: # MacBook Air
     saveStart = '/Users/wrespino/Synced/Remote_Sensing_Projects/A-CCP/canonicalCases4Lille/CASE_' # end will be appended
@@ -44,15 +43,16 @@ bckYAMLpathPOL = os.path.join(ymlDir, 'settings_BCK_POLAR_2modes.yml')
 
 
 
-conCases = ['case06'+caseLet for caseLet in ['a','b','c','d','e','f']]
+conCases = ['case06'+caseLet for caseLet in ['a','b','c','d','e','f']] # 6
 # conCases = ['case06d']
 SZAs = [30] # 3 (GRASP doesn't seem to be wild about θs=0)
 Phis = [0] # 1
 τFactor = [1.0] #2
-# instruments = ['polar07', 'Lidar09+polar07','Lidar05+polar07','Lidar06+polar07',
-#                'polar0700', 'Lidar0900+polar0700','Lidar0500+polar0700','Lidar0600+polar0700'] # 8 N=48
-instruments = ['Lidar0600+polar0700'] # 8 N=42
-rndIntialGuess = True # randomly vary the initial guess of retrieved parameters
+instruments = ['polar07', 'Lidar09+polar07','Lidar05+polar07','Lidar06+polar07',
+               'polar0700', 'Lidar0900+polar0700','Lidar0500+polar0700','Lidar0600+polar0700'] # 8 N=48
+# instruments = ['Lidar0600+polar0700'] # 8 N=42
+rndIntialGuess = n >= 48 # randomly vary the initial guess of retrieved parameters
+if rndIntialGuess: n = n-48 # HACK cycle through case/instrument combos twice, first with default then random initial guesses
 
 paramTple = list(itertools.product(*[instruments,conCases,SZAs,Phis,τFactor]))[n] 
 savePath = saveStart + '%s_%s_sza%d_phi%d_tFct%4.2f_V1.pkl' % paramTple
