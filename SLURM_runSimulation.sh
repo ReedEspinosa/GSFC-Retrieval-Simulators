3cbbc19e6a0004728013a2c966ed34a2ae2cdc86
Reed Espinosa <reed.espinosa@nasa.gov>
2020-08-07 23:38:09 -0400

    mostly logistics regarding bulk processing on DISCOVER

--- SLURM_runSimulation.sh
+++ SLURM_runSimulation.sh
@@ -4,10 +4,11 @@
 #SBATCH --time=01:59:00
 #SBATCH -o log/output.%A-%a
 #SBATCH -e log/error.%A-%a
-#SBATCH --array=0
+#SBATCH --array=0-1
 
 date
 hostname
-echo "---Running Sims---"
-python runRetrievalOSSE.py
+echo "---Running Simulation N="${SLURM_ARRAY_TASK_ID}"---"
+python runRetrievalOSSE.py ${SLURM_ARRAY_TASK_ID}
+
 exit 0
