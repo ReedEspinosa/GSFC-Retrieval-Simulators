7e7f64c9977d2244b4e0b85a8e1eb8a48da21378
Reed Espinosa <reed.espinosa@nasa.gov>
2020-08-28 22:22:17 -0400

    DRS_V01
    based on TEST_V06, with small yaml updates

--- stackSLURM.sh
+++ stackSLURM.sh
@@ -3,18 +3,18 @@
 STARTnAng=0
 date
 hostname
-while [ $STARTnAng -lt 73 ]
+while [ $STARTnAng -lt 100 ]
 do
-    if [ $(squeue -u wrespino | wc -l) -lt 2 ]
+    if [ $(squeue -u wrespino | wc -l) -lt 12 ]
     then
         echo "<><><><><><>"
         date
-        echo "Running Command: sbatch SLURM_runSimulation.sh $STARTnAng"
-        sbatch SLURM_runSimulation.sh $STARTnAng
+        echo "Running Command: sbatch --export=ALL,nAng='$STARTnAng' SLURM_runSimulation.sh"
+        sbatch --export=ALL,nAng='$STARTnAng' SLURM_runSimulation.sh
         squeue -u wrespino
-        STARTnAng=$(($STARTnAng+28))
+        STARTnAng=$(($STARTnAng+1))
         echo "<><><><><><>"
     fi
-    sleep 30
+    sleep 10
 done
 exit 0
