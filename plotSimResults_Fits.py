052be10f04009ec9b314281ebb4067b36d3dded9
Reed Espinosa <reed.espinosa@nasa.gov>
2020-09-01 20:34:44 -0400

    Making plotter work better with OSSE datasets, tried to keep compatiblity with DRS but could be bugs...

--- plotSimResults_Fits.py
+++ plotSimResults_Fits.py
@@ -17,16 +17,21 @@ import matplotlib.pyplot as plt
 
 # simRsltFile can have glob style wildcards
 # simRsltFile = '/Users/wrespino/Synced/Working/SIM16_SITA_JuneAssessment/TEST_V06_Lidar090+polar07_case08h1_tFct1.00_orbSS*_n*_nAng1.pkl'
-simRsltFile = '/Users/wrespino/Synced/Working/SIM17_SITA_SeptAssessment/DRS_V01_Lidar060+polar07_case08a2_tFct1.00_orbSS_multiAngles_n*_nAngALL.pkl'
+nn = int(sys.argv[1])
+mm = int(sys.argv[2])
+simRsltFile = '/Users/wrespino/Synced/Working/SIM_OSSE_Test/gpm-g5nr.leV13.GRASP.YAML*-n%dm%d.polar07*.2006080*_1700z.pkl' % (nn,mm)
 trgtλLidar = 0.532 # μm, note if this lands on a wavelengths without profiles no lidar data will be plotted
-trgtλPolar = 0.550 # μm, if this lands on a wavelengths without I, Q or U no polarimeter data will be plotted
+trgtλPolar = 0.55 # μm, if this lands on a wavelengths without I, Q or U no polarimeter data will be plotted
 extErrPlot = True
+χthresh = .2
+minSaved = 13
+fineModesBck = [0]
 
 # --END INPUT SETTINGS--
 posFiles = glob(simRsltFile)
 assert len(posFiles)==1, 'glob found %d files but we expect exactly 1' % len(posFiles)
 simA = simulation(picklePath=posFiles[0])
-simA.conerganceFilter(χthresh=2000.0, verbose=True, forceχ2Calc=True)
+simA.conerganceFilter(χthresh=χthresh, minSaved=minSaved, verbose=True, forceχ2Calc=True)
 lIndL = np.argmin(np.abs(simA.rsltFwd[0]['lambda']-trgtλLidar))
 lIndP = np.argmin(np.abs(simA.rsltFwd[0]['lambda']-trgtλPolar))
 alphVal = 1/np.sqrt(len(simA.rsltBck))
@@ -62,14 +67,17 @@ if POLARpresent:
     figP, axP = plt.subplots(1,len(measTypesP),figsize=(12,6))
     if not type(axP)==np.ndarray: axP=[axP]
 # Plot LIDAR and Polar measurements and fits
-NfwdModes = simA.rsltFwd[0]['aodMode'].shape[0]
+NfwdModes = simA.rsltFwd[0]['aodMode'].shape[0] if 'aodMode' in simA.rsltFwd[0] else 0
 NbckModes = simA.rsltBck[0]['aodMode'].shape[0]
 frstPass = True
 for rb in simA.rsltBck:
     if LIDARpresent:
+        mdHnd = []
+        lgTxt = []
         for i in range(NbckModes): # Lidar extinction profile
             βprof = norm2absExtProf(rb['βext'][i,:], rb['range'][i,:], rb['aodMode'][i,lIndL])
-            axL[0].plot(1e6*βprof, rb['range'][i,:]/1e3, color=color1[i], alpha=alphVal)
+            mdHnd.append(axL[0].plot(1e6*βprof, rb['range'][i,:]/1e3, color=color1[i], alpha=alphVal))
+            lgTxt.append('Mode %d' % (i+1))
         frstPass = False
         for i,mt in enumerate(measTypesL): # Lidar retrieval meas & fit
             axL[i+1].plot(1e6*rb['meas_'+mt][:,lIndL], rb['RangeLidar'][:,lIndL]/1e3, color=color1[0], alpha=alphVal)
@@ -79,15 +87,12 @@ for rb in simA.rsltBck:
             axP[i].plot(θfun(lIndP,rb), rb['meas_'+mt][:,lIndP], color=color1[0], alpha=alphVal)
             axP[i].plot(θfun(lIndP,rb), rb['fit_'+mt][:,lIndP], color=color1[1], alpha=alphVal)
 if LIDARpresent:
-    mdHnd = []
-    lgTxt = []
     for i in range(NfwdModes):
         βprof = norm2absExtProf(simA.rsltFwd[0]['βext'][i,:], simA.rsltFwd[0]['range'][i,:], simA.rsltFwd[0]['aodMode'][i,lIndL])
-        mdHnd.append(axL[0].plot(1e6*βprof, simA.rsltFwd[0]['range'][i,:]/1e3, 'o-', color=color1[i]/2))
-        axL[0].plot([], [], 'o-', color=color1[i]/2)
-        lgTxt.append('Mode %d' % (i+1))
+        axL[0].plot(1e6*βprof, simA.rsltFwd[0]['range'][i,:]/1e3, 'o-', color=color1[i]/2)
+        axL[0].plot([], [], 'o-', color=color1[i]/2) # ???
     for i,mt in enumerate(measTypesL): # Lidar fwd fit
-        axL[i+1].plot(1e6*simA.rsltFwd[0]['fit_'+mt][:,lIndL], simA.rsltFwd[0]['RangeLidar'][:,lIndL]/1e3, 'ko-')
+        if len(simA.rsltFwd)==1: axL[i+1].plot(1e6*simA.rsltFwd[0]['fit_'+mt][:,lIndL], simA.rsltFwd[0]['RangeLidar'][:,lIndL]/1e3, 'ko-')
         axL[i+1].legend(['Measured', 'Retrieved']) # there are many lines but the first two should be these
     axL[i+1].set_xlim([0,2*1e6*simA.rsltFwd[0]['fit_'+mt][:,lIndL].max()])
 if POLARpresent:
@@ -105,7 +110,11 @@ if LIDARpresent: # touch up LIDAR plots
     axL[0].legend(list(map(list, zip(*mdHnd)))[0], lgTxt)
     axL[0].set_ylabel('Altitude (km)')
     axL[0].set_xlabel('Mode resolved extinction ($Mm^{-1}$)')
-    # axL[0].set_xlim([0,2.0])
+    rngBins = simA.rsltBck[0]['RangeLidar'][:,lIndL]/1e3
+    for ax in axL: ax.set_ylim([rngBins[-1], rngBins[0]])
+    axL[0].set_xlim([0,400])
+    if -np.diff(simA.rsltBck[0]['RangeLidar'][:,lIndL])[0] > -2*np.diff(simA.rsltBck[0]['RangeLidar'][:,lIndL])[-1]: # probably log-spaced range bins
+        for ax in axL: ax.set_yscale('log')
     for i,mt in enumerate(measTypesL): # loop throuh measurement types to label x-axes
         if mt == 'VExt':
             lblStr = 'Total extinction ($Mm^{-1}$)'
@@ -127,8 +136,9 @@ if POLARpresent: # touch up Polarimeter plots
 
 plt.show(block=False)
 
-print(simA.analyzeSim(lIndP)[0])
-print('Total AOD: %f' % simA.rsltFwd[0]['aod'][lIndP])
+print(simA.analyzeSim(lIndP, modeCut=0.5)[0])
+cstV = np.mean([rb['costVal'] for rb in simA.rsltBck])
+print('Total AOD: %f | Cost Value: %f' % (simA.rsltFwd[0]['aod'][lIndP], cstV))
 
 # For X11 on Discover
 # plt.ioff()
