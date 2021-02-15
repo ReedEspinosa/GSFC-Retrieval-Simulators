781b8067832a104bb8db59965fa925cfa83e9830
Reed Espinosa <reed.espinosa@nasa.gov>
2021-02-15 15:52:56 -0500

    state when basic01 was generated

--- SLURM_runSimulation.sh
+++ SLURM_runSimulation.sh
@@ -1,18 +1,27 @@
 #!/usr/local/bin/bash
-#SBATCH --job-name=GOSSE
+#SBATCH --job-name=BASIC
 #SBATCH --nodes=1 --constraint=hasw
-#SBATCH --time=00:04:30
+#SBATCH --time=00:25:00
 #SBATCH -o log/output.%A-%a
 #SBATCH -e log/error.%A-%a
-#SBATCH --array=0-9
+#SBATCH --array=1-2
 
 date
 hostname
 echo "---Running Simulation N="${SLURM_ARRAY_TASK_ID}"---"
-python runRetrievalSimulation.py 0 1${SLURM_ARRAY_TASK_ID} &
-python runRetrievalSimulation.py 0 2${SLURM_ARRAY_TASK_ID} &
-python runRetrievalSimulation.py 0 3${SLURM_ARRAY_TASK_ID} &
-python runRetrievalSimulation.py 0 4${SLURM_ARRAY_TASK_ID} &
-python runRetrievalSimulation.py 0 5${SLURM_ARRAY_TASK_ID} &
+python runRetrievalSimulation.py 0 $((${SLURM_ARRAY_TASK_ID}+0)) &
+python runRetrievalSimulation.py 0 $((${SLURM_ARRAY_TASK_ID}+3)) &
+python runRetrievalSimulation.py 0 $((${SLURM_ARRAY_TASK_ID}+6)) &
+python runRetrievalSimulation.py 0 $((${SLURM_ARRAY_TASK_ID}+9)) &
+python runRetrievalSimulation.py 0 $((${SLURM_ARRAY_TASK_ID}+12)) &
+python runRetrievalSimulation.py 0 $((${SLURM_ARRAY_TASK_ID}+15)) &
+python runRetrievalSimulation.py 0 $((${SLURM_ARRAY_TASK_ID}+18)) &
+python runRetrievalSimulation.py 0 $((${SLURM_ARRAY_TASK_ID}+21)) &
+python runRetrievalSimulation.py 0 $((${SLURM_ARRAY_TASK_ID}+24)) &
+python runRetrievalSimulation.py 0 $((${SLURM_ARRAY_TASK_ID}+27)) &
+python runRetrievalSimulation.py 0 $((${SLURM_ARRAY_TASK_ID}+31)) &
+python runRetrievalSimulation.py 0 $((${SLURM_ARRAY_TASK_ID}+34)) &
+python runRetrievalSimulation.py 0 $((${SLURM_ARRAY_TASK_ID}+37)) &
+python runRetrievalSimulation.py 0 $((${SLURM_ARRAY_TASK_ID}+41)) &
 wait
 exit 0
