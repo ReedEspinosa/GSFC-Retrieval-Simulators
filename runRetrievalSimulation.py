53645422a516c17d9c4fa8dd197969ec8aa1b04c
Reed Espinosa <reed.espinosa@nasa.gov>
2021-02-15 15:54:33 -0500

    state when multipixel10 was generated

--- runRetrievalSimulation.py
+++ runRetrievalSimulation.py
@@ -19,19 +19,19 @@ if checkDiscover(): # DISCOVER
     n = int(sys.argv[1]) # (0,1,2,...,N-1)
     nAng = int(sys.argv[2])
     basePath = os.environ['NOBACKUP']
-    saveStart = os.path.join(basePath, 'synced/Working/TASNPP_simulation00/Test52_')
-    ymlDir = os.path.join(basePath, 'MADCAP_scripts/ACCP_ArchitectureAndCanonicalCases/')
+    saveStart = os.path.join(basePath, 'synced/Working/TASNPP_simulation00/MultiPix10_')
+    ymlDir = os.path.join(basePath, 'MADCAP_Analysis_multiPix/ACCP_ArchitectureAndCanonicalCases/')
     dirGRASP = os.path.join(basePath, 'grasp_open/build/bin/grasp')
     krnlPath = os.path.join(basePath, 'local/share/grasp/kernels')
     geomFile = os.path.join(basePath, 'synced/Working/NASA_Ames_MOD_angles-SZA-VZA-PHI.txt')
     Nangles = 16
-    Nsims = 4 # number of runs (if initial guess is not random this just varies the random noise)
-    maxCPU = 4 # number of cores to divide above Nsims over... we might need to do some restructuring here
+    Nsims = 2 # number of runs (if initial guess is not random this just varies the random noise)
+    maxCPU = 2 # number of cores to divide above Nsims over... we might need to do some restructuring here
 else: # MacBook Air
     n = 0
     nAng = 11
     saveStart = '/Users/wrespino/Desktop/TEST_V03_' # end will be appended
-    ymlDir = '/Users/wrespino/Synced/Local_Code_MacBook/MADCAP_Analysis/ACCP_ArchitectureAndCanonicalCases/'
+    ymlDir = '/Users/wrespino/Synced/Local_Code_MacBook/MADCAP_Analysis_multiPix/ACCP_ArchitectureAndCanonicalCases/'
     dirGRASP = '/Users/wrespino/Synced/Local_Code_MacBook/grasp_open/build/bin/grasp'
     geomFile = '/Users/wrespino/Synced/Proposals/ROSES_TASNPP_Yingxi_2020/retrievalSimulation/NASA_Ames_MOD_angles-SZA-VZA-PHI.txt'
     krnlPath = None
@@ -41,8 +41,8 @@ else: # MacBook Air
 fwdModelYAMLpathLID = os.path.join(ymlDir, 'settings_FWD_POLARandLIDAR_1lambda.yml')
 bckYAMLpathLID = os.path.join(ymlDir, 'settings_BCK_POLARandLIDAR_10Vbins_2modes.yml') # will get bumped to 4 modes if needed
 fwdModelYAMLpathPOL = os.path.join(ymlDir, 'settings_FWD_IQU_POLAR_1lambda.yml')
-# bckYAMLpathPOL = os.path.join(ymlDir, 'settings_BCK_POLAR_2modes.yml')
-bckYAMLpathPOL = os.path.join(ymlDir, 'settings_BCK_POLAR_2modes_DTarbsorb.yml')
+bckYAMLpathPOL = os.path.join(ymlDir, 'settings_BCK_POLAR_2modes.yml')
+#bckYAMLpathPOL = os.path.join(ymlDir, 'settings_BCK_POLAR_2modes_DTarbsorb.yml')
 
 instruments = ['modis']
 conCases = ['HuamboVegetation', 'NasaAmesVegetation'] # a1,a2,b1,..,o2 #180
